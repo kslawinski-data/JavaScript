0957f532ebe15b77d3c63e4081affcb19a31eefc
kslawinski-data <kslawinski.info@gmail.com>
2025-09-22 20:03:17 +0200

    Add files via upload

---
+++ js7/script.js
@@ -0,0 +1,13 @@
+
+const car = {
+  brand: "Toyota",
+  model: "Corolla",
+  color: "Black",
+  year: 2021
+};
+
+console.log("Car Information:");
+console.log("Brand:", car.brand);
+console.log("Model:", car.model);
+console.log("Color:", car.color);
+console.log("Year of production:", car.year);
