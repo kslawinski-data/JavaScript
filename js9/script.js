4050c6244d84846275b8fcdcd845ddbb033b7821
kslawinski-data <kslawinski.info@gmail.com>
2025-09-24 08:34:33 +0200

    Add files via upload
    This script creates an animal object, displays its properties, and then shows the sound it makes — all done using a function.

---
+++ js9/script.js
@@ -0,0 +1,25 @@
+const animal = {
+	species: "dog",
+	name: "cloud",
+	age: 3,
+	sound: "woof woof"
+}
+
+function displayAnimal(obj) {
+  console.log("Animal content:");
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      console.log(`${key}: ${obj[key]}`);
+    }
+  }
+}
+
+function displaySound(obj) {
+	console.log("Sound of animal:");
+	console.log(animal['sound']);
+}
+
+
+
+displayAnimal(animal);
+displaySound(animal);
